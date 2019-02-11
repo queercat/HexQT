02975b0869b9900f7e92dd088b7b2bce13066cd5
May Tusek <tusek.nicole@gmail.com>
2019-02-11 12:22:31 -0800

    Created basic windowed application.

---
+++ hexqt.py
@@ -0,0 +1,29 @@
+# hexqt.py -- HexQT a pretty QT hext editor.
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
+        self.initUI()
+    
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.show()
+
+def main():
+    app = QApplication(sys.argv)    
+    ex = App()
+    sys.exit(app.exec_())
+
+# Initialize the brogram.
+if __name__ == '__main__':
+    main()
